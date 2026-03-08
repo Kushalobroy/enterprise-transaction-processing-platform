a1282127b9e52212c543760a170533f466d17cd6
Kushal <kushalobroy@gmail.com>
2026-03-08 11:16:22 +0530

    initial

---
+++ edprs/src/main/java/com/kushal/edprs/EdprsApplication.java
@@ -0,0 +1,13 @@
+package com.kushal.edprs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdprsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdprsApplication.class, args);
+	}
+
+}
---
+++ edprs/src/test/java/com/kushal/edprs/EdprsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushal.edprs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdprsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
